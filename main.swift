4e5a0d8e17fd1bd126621e730ebe8b15406a6f3b
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-30 21:19:51 +0500

    hello world : )

--- main.swift
+++ main.swift
@@ -1,1 +1,42 @@
-print("Hello, World!")
+
+import Foundation
+
+// var n constants
+
+var num = 10;
+// num = "HI";
+print(num);
+
+let pi = 3.14;
+// pi = 2; // error
+
+// main.swift:11:1: error: cannot assign to value: 'pi' is a 'let' constant
+//  8 | print(num);
+//  9 | 
+// 10 | let pi = 3.14;
+//    | `- note: change 'let' to 'var' to make it mutable
+// 11 | pi = 2; // error
+//    | `- error: cannot assign to value: 'pi' is a 'let' constant
+// 12 | print(pi);
+
+print(pi);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
